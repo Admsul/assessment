85d89b584ca6a145a7808798fa411320d4a3d405
Adam Sullivan <adam_michael_sullivan@hotmail.co.uk>
2025-07-12 10:02:19 +0100

    Set up, added in folders and basic files

---
+++ javascript/scripts.js
@@ -0,0 +1,4 @@
+// on page load to set the current year in the footer
+window.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('year').textContent = new Date().getFullYear();
+});
