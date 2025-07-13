fcc7d4e7d31bf86ddb1590643fd3a6603c4a1c1f
Adam Sullivan <adam_michael_sullivan@hotmail.co.uk>
2025-07-13 17:47:09 +0100

    More Css changes and form edits

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,8 @@
 // on page load to set the current year in the footer
 window.addEventListener('DOMContentLoaded', function() {
     document.getElementById('year').textContent = new Date().getFullYear();
+    document.getElementById("form").reset();
+
 });
 
 function validate() {
